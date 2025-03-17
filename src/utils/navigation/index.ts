4f1b2c1662372d7e37f4e8f38198dfc7d6283246
mariobalrod <marioballestero.rdg@gmail.com>
2025-03-17 17:53:21 +0100

    refactor: remove unused code

--- src/utils/navigation/index.ts
+++
@@ -1,9 +0,0 @@
-import { createNavigation } from 'next-intl/navigation';
-
-import { DEFAULT_LOCALE, LOCALES } from '@/i18n/locales';
-
-export const { Link, redirect, usePathname, useRouter } = createNavigation({
-  locales: LOCALES,
-  defaultLocale: DEFAULT_LOCALE,
-  localePrefix: 'always',
-});
